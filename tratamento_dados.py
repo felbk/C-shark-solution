6d40200c4f022c209f66bd83abc196b0e1cfc625
Felipe Lambak <Lambakfelipe@gmail.com>
2024-08-25 15:34:11 -0300

    Inicio base de dados manual para cupons

--- tratamento_dados.py
+++ tratamento_dados.py
@@ -45,14 +45,12 @@ def trata_dados(document_id, client_id):
 #Data base para treino do gerador de cupom
 
 
-Cupons = {'UpMean': [0.83, 0.52, 0.26, 0.53, 0.26, 0.05, 0.27, 0.76, 0.43, 0.39, 0.69, 0.76, 0.14, 0.19, 0.76, 0.36, 0.66, 0.79, 0.89, 0.07, 0.47, 0.39, 0.22, 0.96, 0.94, 0.23, 0.15, 0.87, 0.43, 0.66, 0.94, 0.14, 0.93, 0.88, 0.79, 0.54, 0.26, 0.31, 0.72, 0.07, 0.32, 0.79, 0.41, 0.81, 0.07, 0.6, 0.49, 0.82, 0.27, 0.45, 0.79, 0.23, 0.78, 0.99, 0.44, 0.48, 0.02, 0.37, 0.82, 0.22, 0.56, 0.49, 0.42, 0.01, 0.51, 0.56, 0.99, 0.87, 0.79, 0.78, 0.83, 0.95, 0.72, 0.29, 0.87, 0.3, 0.34, 0.84, 0.54, 0.97, 0.46, 0.77, 0.01, 0.76, 0.89, 0.53, 0.06, 0.1, 0.8, 0.04, 0.52, 0.65, 0.03, 0.96, 0.88, 0.64, 0.5, 0.08, 0.85, 0.41],
+Cupons = {'UpMean': [0.5,0.8,1],
            
-'OffPrice': [0.3, 0.47, 0.17, 0.03, 0.11, 0.42, 0.45, 0.33, 0.4, 0.49, 0.18, 0.3, 0.45, 0.47, 0.38, 0.36, 0.34, 0.06, 0.17, 0.16, 0.47, 0.48, 0.23, 0.23, 0.28, 0.31, 0.25, 0.25, 0.4, 0.15, 0.31, 0.36, 0.17, 0.21, 0.18, 0.18, 0.09, 0.25, 0.35, 0.43, 0.06, 0.36, 0.27, 0.22, 0.4, 0.07, 0.36, 0.38, 0.44, 0.37, 0.3, 0.3, 0.02, 0.25, 0.17, 0.45, 0.02, 0.02, 0.35, 0.36, 0.28, 0.11, 0.01, 0.34, 0.27, 0.1, 0.42, 0.12, 0.39, 0.33, 0.04, 0.12, 0.26, 0.43, 0.05, 0.16, 0.1, 0.33, 0.44, 0.15, 0.34, 0.12, 0.46, 0.09, 0.05, 0.47, 0.36, 0.11, 0.1, 0.35, 0.3, 0.09, 0.47, 0.07, 0.02, 0.18, 0.13, 0.15, 0.18, 0.16],
-"Day" : ['Thursday', 'Friday', 'Monday', 'Monday', 'Sunday', 'Saturday', 'Tuesday', 'Wednesday', 'Sunday', 'Thursday', 'Thursday', 'Wednesday', 'Thursday', 'Wednesday', 'Friday', 'Saturday', 'Wednesday', 'Thursday', 'Wednesday', 'Friday', 'Saturday', 'Sunday', 'Sunday', 'Monday', 'Sunday', 'Tuesday', 'Monday', 'Monday', 'Monday', 'Thursday', 'Thursday', 'Wednesday', 'Sunday', 'Saturday', 'Tuesday', 'Thursday', 'Tuesday', 'Friday', 'Friday', 'Thursday', 'Tuesday', 'Monday', 'Saturday', 'Saturday', 'Sunday', 'Saturday', 'Wednesday', 'Monday', 'Sunday', 'Wednesday', 'Saturday', 'Thursday', 'Tuesday', 'Thursday', 'Sunday', 'Sunday', 'Monday', 'Tuesday', 'Sunday', 'Tuesday', 'Wednesday', 'Thursday', 'Wednesday', 'Friday', 'Sunday', 'Friday', 'Tuesday', 'Wednesday', 'Monday', 'Tuesday', 'Sunday', 'Thursday', 'Friday', 'Saturday', 'Friday', 'Monday', 'Saturday', 'Wednesday', 'Friday', 'Friday', 'Wednesday', 'Thursday', 'Monday', 'Thursday', 'Tuesday', 'Sunday', 'Thursday', 'Wednesday', 'Wednesday', 'Monday', 'Tuesday', 'Friday', 'Tuesday', 'Wednesday', 'Monday', 'Monday', 'Sunday', 'Thursday', 'Tuesday', 'Tuesday'],
+'OffPrice': [0.2,0.1,0.3],
+"Day" : ["Sunday","Saturday","Monday"],
 
- 'Used': [True, True, False, False, 
-False, False, True, True, False, True, False, True, True, True, False, True, True, True, False, False, True, False, True, False, True, False, False, True, True, True, True, False, True, True, True, False, False, True, True, False, True, True, False, True, False, False, True, True, False, False, False, False, True, True, False, False, False, False, False, False, False, False, False, False, True, False, True, False, False, False, True, False, True, False, True, True, False, False, True, True, True, True, False, True, False, True, 
-False, False, True, True, False, False, False, True, True, True, True, False, False, False]}
+ 'Used': [True,False,True]}
 
 Cupons = pd.DataFrame(Cupons)
 
@@ -151,7 +149,7 @@ def gera_cupom(df_pred):
     Cupom = f"Cupom de R${(Mean*(1+UpMean)*OffPrice):.2f} em compras acima de R${(Mean*(1+UpMean)):.2f} para usar {translate[day]}!"
     # Exibir resultados
     print(df_predict)
-    
+
     return Cupom
     
 
