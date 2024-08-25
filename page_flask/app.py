4fdaa1e0bfbc4e025153d585169cea8a460de2db
Felipe Lambak <Lambakfelipe@gmail.com>
2024-08-25 19:18:00 -0300

    atualiza grafico ao entrar no app

--- page_flask/app.py
+++ page_flask/app.py
@@ -1,4 +1,13 @@
 from flask import Flask, render_template, request
+from GeraGrafico import gerarGraficos
+from tratamento_dados import calcula_total , gera_cupom
+import pandas as pd
+
+document_id= "453832840298988785"
+dfTotal = pd.DataFrame(calcula_total(document_id))
+x= ["Dom","Seg","Ter","Qua","Qui","Sex","Sab"]
+y= dfTotal["sum"]
+gerarGraficos("static\images\GraficoSemanal.png",x,y)
 
 app = Flask(__name__)
 
@@ -17,3 +26,4 @@ def processar():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
