2377e4358f1b3648a1cb1ae83ecb67e8ca91def0
Felipe Lambak <Lambakfelipe@gmail.com>
2024-08-25 22:52:47 -0300

    ativar e desativar

--- page_flask/static/js/script.js
+++ page_flask/static/js/script.js
@@ -1,10 +1,16 @@
 const openPopupButton = document.getElementById('open_popup');
 const popup = document.getElementById('popup');
 const closePopupButton = document.getElementById('close_popup');
-
+const textPopup = document.getElementById('linkid');
 // Função para abrir o pop-up
 openPopupButton.addEventListener('click', () => {
-    popup.style.display = 'flex';
+    if (textPopup.textContent == "Ativar"){
+    popup.style.display = 'flex';}
+
+    if (textPopup.textContent=='Ativar'){
+    textPopup.textContent= 'Desativar'; } else { 
+     textPopup.textContent='Ativar';
+    }
 });
 
 // Função para fechar o pop-up
